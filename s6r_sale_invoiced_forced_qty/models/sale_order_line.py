c4de752b3784e1b337f301212477abc3970a02e8
superColson <colin.chibois@gmail.com>
2025-01-22 11:18:16 +0100

    [ADD] added a field to force invoiced amount on SO line, added calculation of amount_to_invoice on SO

--- s6r_sale_invoiced_forced_qty/models/sale_order_line.py
+++ s6r_sale_invoiced_forced_qty/models/sale_order_line.py
@@ -9,10 +9,23 @@ class SaleOrderLine(models.Model):
         help= "Qty already invoiced but not related to existing invoices in Odoo (for history data import purposes)",
         digits='Product Unit of Measure')
 
+    untaxed_amount_invoiced_forced = fields.Monetary(
+        string="Forced Invoiced Amount",
+        help="Amount already invoiced but not related to existing invoices in Odoo (for history data import purposes).\
+        If this field is left blank, the pre-existing amount will not be taken in consideration when calculating the \
+        total invoiced amount for the sale order. Therefore, reports relating to this sale order will be false."
+    )
 
-    @api.depends('invoice_lines.move_id.state', 'invoice_lines.quantity', 'qty_invoiced_forced')
+    @api.depends('qty_invoiced_forced')
     def _compute_qty_invoiced(self):
         super()._compute_qty_invoiced()
         for line in self:
             line.qty_invoiced += line.qty_invoiced_forced
+        return
+
+    @api.depends('untaxed_amount_invoiced_forced')
+    def _compute_untaxed_amount_invoiced(self):
+        super()._compute_untaxed_amount_invoiced()
+        for line in self:
+            line.untaxed_amount_invoiced += line.untaxed_amount_invoiced_forced
         return
